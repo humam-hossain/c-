956eaac8d64113963f5eff9764a3054224c1d79c
humam-hossain <66829442+humam-hossain@users.noreply.github.com>
2021-04-10 00:09:01 +0600

    Add files via upload

---
+++ codejam/codejam.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(int t) {
+  cout << "Case #" << t << ": ";
+  
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  int T;
+  cin >> T;
+  for (int t = 1; t <= T; t++) solve(t);
+}
