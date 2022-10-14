1e7065ee873e741bd2b4827918c5b0a99c7ecb2d
humam-hossain <humam.hossain.e@gmail.com>
2022-10-14 22:54:16 +0600

    prob_3 done

---
+++ lab4/src/prob_3.c
@@ -0,0 +1,22 @@
+// fibonacci series
+// 0+1+1+2+3+5+8+.....+Nth term
+
+#include<stdio.h>
+
+int main()
+{
+    int i, n, f=0, s=1, temp, sum=f;
+
+    scanf("%d", &n);
+
+    for(i=1; i<n; i++){
+        sum += s;
+        temp = f + s;
+        f = s;
+        s = temp;
+    }
+
+    printf("= %d", sum);
+
+    return 0;
+}
