33e46780606da7c893b5f2a8e83d3c60be7c42e9
humam-hossain <humam.hossain.e@gmail.com>
2022-10-22 02:55:11 +0600

    revised

--- friends' projects/sadeed/dsa/src/bubbleSort.cpp
+++ friends' projects/sadeed/dsa/src/bubbleSort.cpp
@@ -3,29 +3,12 @@
 #include <ctime>
 #include <fstream>
 
-#define array1_size 100000
-#define array2_size 50000
+#define array1_size 200000
+#define array2_size 100000
 #define array3_size 10000
-#define array4_size 500
-
+#define array4_size 5000
 
 using namespace std;
-
-// bool binarySearch(int *p, int low, int high, int V)
-// {
-//     int mid;
-//     while (low <= high)
-//     {
-//         mid = (low + high) / 2;
-//         if (*(p + mid) == V)
-//             return true;
-//         else if (*(p + mid) < V) // now, you explore the right subarray w.r.t mid point
-//             low = mid + 1;
-//         else
-//             high = mid - 1; // exploring the left subarray w.r.t. mid point
-//     }
-//     return false; // V has not been found
-// }
 
 void bubbleSort(int *p, int n)
 {
@@ -46,10 +29,11 @@ void bubbleSort(int *p, int n)
 string print(int *p, int n)
 {
     string result = "";
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         result += to_string(*(p + i)) + "\t";
 
-        if((i+1)%20 == 0)
+        if ((i + 1) % 20 == 0)
             result += "\n";
     }
 
@@ -65,7 +49,7 @@ string check(int *array, int n)
     ios_base::sync_with_stdio(false);
 
     bubbleSort(array, n);
-    
+
     time(&end);
 
     result += "Time taken => " + to_string(double(end - start)) + " sec \n\n\n";
@@ -100,22 +84,22 @@ int main()
     int n;
 
     n = sizeof(array1) / sizeof(array1[0]);
-    ofstream file1("array1_bubbleSorted.txt");
+    ofstream file1("../data/array1_bubbleSorted.txt");
     file1 << check(array1, n);
     file1.close();
 
     n = sizeof(array2) / sizeof(array2[0]);
-    ofstream file2("array2_bubbleSorted.txt");
+    ofstream file2("../data/array2_bubbleSorted.txt");
     file2 << check(array2, n);
     file2.close();
-    
+
     n = sizeof(array3) / sizeof(array3[0]);
-    ofstream file3("array3_bubbleSorted.txt");
+    ofstream file3("../data/array3_bubbleSorted.txt");
     file3 << check(array3, n);
     file3.close();
 
     n = sizeof(array4) / sizeof(array4[0]);
-    ofstream file4("array4_bubbleSorted.txt");
+    ofstream file4("../data/array4_bubbleSorted.txt");
     file4 << check(array4, n);
     file4.close();
 
