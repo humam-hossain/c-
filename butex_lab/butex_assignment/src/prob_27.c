2e96f43b3d220a880b0e374521ed4e55aec89c58
humam-hossain <humam.hossain.e@gmail.com>
2022-11-01 03:31:49 +0600

    done

--- butex_lab/butex_assignment/src/prob_27.c
+++ butex_lab/butex_assignment/src/prob_27.c
@@ -1,9 +1,33 @@
 //  Write a c program to find out the sum of an A.P. series.
+// Sn = n/2 * {2a + (n-1)*d}
 
 #include<stdio.h>
 
 int main()
 {
+	int a, d, n, sum, i, t;
+
+	printf("a = ");
+	scanf("%d", &a);
+	printf("d = ");
+	scanf("%d", &d);
+	printf("n = ");
+	scanf("%d", &n);
+
+	sum = 0;
+	t = a;
+	for(i=1; i<=n; i++){
+		printf("%d ", t);
+		sum += t;
+
+		if(i!=n){
+			printf("+ ");
+		}
+		t += d;
+	}
+
+	// sum = (int)(n/2.0 * (2*a + (n-1) * d));
+	printf(" = %d", sum);
 
 	return 0;
 }
--- butex_lab/butex_assignment/src/prob_28.c
+++ butex_lab/butex_assignment/src/prob_28.c
@@ -4,6 +4,29 @@
 
 int main()
 {
+	int a, r, n, sum, i, t;
+
+	printf("a = ");
+	scanf("%d", &a);
+	printf("r = ");
+	scanf("%d", &r);
+	printf("n = ");
+	scanf("%d", &n);
+
+	sum=0;
+	t=a;
+	for(i=1; i<=n; i++){
+		printf("%d ", t);
+
+		if(i != n){
+			printf("+ ");
+		}
+
+		sum += t;
+		t *= r;
+	}
+
+	printf("= %d", sum);
 
 	return 0;
 }
