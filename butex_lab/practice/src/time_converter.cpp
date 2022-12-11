bef5683574c11fdd6599a57cad74c2a0c3907bc7
humam-hossain <humam.hossain.e@gmail.com>
2022-12-11 21:22:36 +0600

    time converter second method

--- butex_lab/practice/src/time_converter.cpp
+++ butex_lab/practice/src/time_converter.cpp
@@ -36,3 +36,65 @@ int main()
 
     return 0;
 }
+
+/* 2nd method
+
+#include<stdio.h>
+#include<string.h>
+
+void convert_time(char time_12[], char time_24[])
+{
+    strcpy(time_24, time_12);
+
+    if(time_24[5] == 'A' || time_24[5] == 'a'){
+        if(time_24[0] == '1' && time_24[1] == '2'){
+            time_24[0] = '0';
+            time_24[1] = '0';
+        }
+    }else{
+        if(time_24[0] == '1'){
+            if(time_24[1] == '0'){  // 10:XXPM -> 22:XX
+                time_24[0] = '2';
+                time_24[1] = '2';
+            }else if(time_24[1] == '1'){ //11:XXPM -> 23:XX
+                time_24[0] = '2';
+                time_24[1] = '3';
+            }
+        }else{
+            if(time_24[1] == '8'){ // 08:XXPM -> 20:XX
+                time_24[0] = '2';
+                time_24[1] = '0';
+            }else if(time_24[1] == '9'){ // 09:XXPM -> 21:XX
+                time_24[0] = '2';
+                time_24[1] = '1';
+            }else{ //07:XXPM -> 19:XX
+                time_24[0] = '1';
+                time_24[1] = time_24[1] + 2;
+            }
+        }
+    }
+
+    time_24[5] = '\0';
+}
+
+int main()
+{
+    // 12:00AM -> 00:00
+    // 03:30PM -> 15:30
+    // 03:00AM -> 03:00
+    // 12:00PM -> 12:00
+    // 08:00PM -> 20:00
+
+    char time_12[7];
+    char time_24[7];
+
+    scanf("%s", time_12);
+
+    convert_time(time_12, time_24);
+
+    printf("%s", time_24);
+
+    return 0;
+}
+
+*/
